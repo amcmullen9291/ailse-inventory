2b271e70f5cba393f3a546585a67d0876bd134f2
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-28 17:30:03 -0500

    grouping error in layouts/app.html.erb

--- app/models/aisle.rb
+++ app/models/aisle.rb
@@ -4,6 +4,6 @@ class Aisle < ApplicationRecord
     has_many :manufacturers, through: :cards
 
     validates_presence_of :aisle_number
-    validates_uniqueness_of :aisle_number, :message => "Aisle already exists"
+    validates_uniqueness_of :aisle_number, :message => "Already Entered"
     validates :aisle_number, numericality: true
 end
--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -2,4 +2,5 @@ class Manufacturer < ApplicationRecord
     has_many :cards 
     has_many :aisles, through: :cards
     validates_presence_of :name 
+    validates_uniqueness_of :name, :message => "Already Listed"
 end
