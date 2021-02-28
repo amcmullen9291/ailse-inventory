ccda2f18e067bbb9d8bbd001198955c48b4a3ecf
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-28 18:49:27 -0500

    added accepts_nested_attributes_for :name_of

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -2,5 +2,6 @@ class Occasion < ApplicationRecord
     has_many :cards
     has_many :aisles, through: :cards 
 
-    validates_presence_of :name_of
+    validates_presence_of :name_of, length: { minimum: 5 }
+    accepts_nested_attributes_for :name_of
 end
