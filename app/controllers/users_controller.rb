4ad79b690411a131744b5a1f2c1c7da30b64bc21
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-28 12:28:38 -0500

    added form_for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
 
     def welcome 
+        @user = User.new
     end 
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+
+        else
+            render :new
+        end
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:access_id, :store_id, :notes, :EmployeeInIt)
+    end     
 end
