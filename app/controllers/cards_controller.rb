b2b242b99f42b1982de7b92340054199f4f979ab
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-28 20:27:22 -0500

    added active_storage for pics

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,26 @@
 class CardsController < ApplicationController
+    def new 
+    end
+
+    def create 
+    end 
+
+    def edit 
+    end 
+
+    def update 
+    end 
+
+    def show 
+    end 
+
+    def destroy
+    end 
+
+    def index 
+    end 
+
+    private 
+    def card_params
+        params.require(:card).permit(:desription, :in_stock, :aisle_id, :occasion_id, :sku, :manufacturer_id, :avatar)
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -7,7 +7,7 @@ class Card < ApplicationRecord
     validates_presence_of :occasion_id
     validates_presence_of :in_stock, numericality: { only_integer: true }
     validates_presence_of :description, length: { maximum: 150 }
-
+    has_one_attached :avatar
     accepts_nested_attributes_for :name_of, allow_destroy: true
 
 end
